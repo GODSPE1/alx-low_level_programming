0b572580e07880155d8b92639bd3d0595a1aff63
Godspower <godspy18@gmail.com>
2023-06-15 21:31:46 -0700

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*This the main function, it describes about the function*/
+/**
+ * main -  function, it describes about the function
+ *
+ * Return: (0);
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
