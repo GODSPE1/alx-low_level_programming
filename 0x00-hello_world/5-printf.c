3436312c2052a34229b1d461e82030f18b53aadd
Godspower <godspy18@gmail.com>
2023-06-15 20:58:14 -0700

    Done the code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This the main function, the entry point.
+ */
+int main(void)
+{
+	/*code for prograns is here*/
+	printf("with  proper grammer, but the outcome is a piece of art\n");
+	/* always 0 (success)*/
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d  bytes(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(nt));
+	printf("size of a long int: %d byte(S)", sizeof(long int));
+	printf("Size of a long long int: %d  byte(S)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
