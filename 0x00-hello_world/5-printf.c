933eecc5559d42404e5d8402060d9ed4d8905b39
Godspower <godspy18@gmail.com>
2023-06-15 21:14:29 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * This the main function, the entry point.
+ * main - function, the entry point.
+ * Return: (0);
  */
 int main(void)
 {
-	/*code for prograns is here*/
+	/*codefor  prograns is here*/
 	printf("with  proper grammer, but the outcome is a piece of art\n");
 	/* always 0 (success)*/
 	return (0);
