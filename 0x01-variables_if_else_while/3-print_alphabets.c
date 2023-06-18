518c1692c385d76d32f074d362fa4d90f7aef8d1
Godspower <godspy18@gmail.com>
2023-06-17 21:49:57 -0700

    lower and uppercase alphabeths

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: Print lower and upper case
+ *Return: 0 means success
+ */
+	int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar (d);
+		d++;
+	}
+	putchar('c');
+	return (0);
+}
