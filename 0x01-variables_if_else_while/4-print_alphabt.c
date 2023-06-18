31179208e836fa827133f560dc4f6ec314bac1ad
Godspower <godspy18@gmail.com>
2023-06-18 09:07:05 -0700

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+/**
+ *main - Entry point
+ *Description:
+ *Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
