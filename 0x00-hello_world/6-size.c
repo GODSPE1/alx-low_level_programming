d51d1e47aee51812b423efa03ee3bbd6e2d52e0b
Godspower <godspy18@gmail.com>
2023-06-17 17:12:41 -0700

    donee

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main-Entry point
- *
- * Return: always 0 (success)
+ * main - displays the size of each data type
+ * Return: return 0
  */
-int main(void)
+	int main(void)
 {
-	printf("Size of a char: %li  bytes(s)\n", sizeof(char));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
-	printf("size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %li  byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long));
 	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
