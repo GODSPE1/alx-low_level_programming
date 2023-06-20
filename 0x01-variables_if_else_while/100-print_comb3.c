1ec5d8e261955c491bc6b3b32e063a81c6bf823e
Godspower <godspy18@gmail.com>
2023-06-19 17:57:53 -0700

    differnt combination of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = i + 1;
+
+		while (j < 10)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i != 8 || i != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
