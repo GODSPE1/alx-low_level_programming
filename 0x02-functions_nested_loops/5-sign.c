1eff1bc0709fc7a7aea1619268d093d65283a529
Godspower <godspy18@gmail.com>
2023-06-21 10:59:21 -0700

    checking for lower and uppercase letters

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		-purchar(43);
+		purchar(43);
 		return (1);
 	}
 	else if (n < 0)
@@ -23,5 +23,5 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
