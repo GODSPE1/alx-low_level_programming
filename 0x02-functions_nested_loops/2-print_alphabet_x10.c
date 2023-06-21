68c7ab3965dfd3604f94ee288c88a9abfaf955e7
Godspower <godspy18@gmail.com>
2023-06-20 17:35:24 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
