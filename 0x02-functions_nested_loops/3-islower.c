442fce0db36cad51181c59265d761d0cd03bff4a
Godspower <godspy18@gmail.com>
2023-06-21 12:44:07 -0700

    checking for lower and uppercase letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,16 @@
 #include "main.h"
 /**
- *int _islower(int c) - check if char is lower case
- *
- *
- *@c: Character of the ASCII code
+ * _islower - check if char is lower case
+ * @c: Character of the ASCII code
  *
  * Return: 1 for lower case and Return 0 for others
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 
-		return (1);
+	return (1);
 
 	else
-
-		return (0);
-
-	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,10 @@
 /**
  * main - check the code.
  *@n: represent the integer
- *
+ *This function takes an integer as input and prints the sign of the number.
+ * If the number is positive, it prints a plus sign (+).
+ * If the number is negative, it prints a minus sign (-).
+ * If the number is zero, it prints a zero (0).
  * Return: Always 0.
  */
 int print_sign(int n)
