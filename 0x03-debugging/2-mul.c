a7f2867bf2bff3dd009f7d7c1becdca0c153fb37
Godspower <godspy18@gmail.com>
2023-06-22 18:45:14 -0700

    multiplies two integers

---
+++ 0x03-debugging/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+	* mul - multiplies two integer
+	* @a: first input
+	* @b: second input
+	* Return: Always 0.
+	*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
