3258efa76619a6311270685c20597a11c2cbf474
GODSPE1 <godspy18@gmail.com>
2023-10-02 19:28:25 -0700

    created a static library

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _puts - prints eacl letter
+ * Return: Always 0.
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
