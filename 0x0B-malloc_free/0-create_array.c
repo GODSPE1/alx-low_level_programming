6e2e3db4179567a5acb613c64a98ce81786ac578
GODSPE1 <godspy18@gmail.com>
2023-10-04 01:44:35 -0700

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc((size + 1) * sizeof(char));
+	ptr = malloc(size * sizeof(char));
 	if (ptr !=  NULL)
 	{
 		for (i = 0; i < size; i++)
