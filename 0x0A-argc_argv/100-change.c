4873e712ee433577502b2937b1c43b5636c47f04
GODSPE1 <godspy18@gmail.com>
2023-10-07 15:16:04 -0700

    print minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char argv[])
+{
+int q = 0;
+int d = 0;
+int n = ;
+int p = 0;
+int cents = 0;
+
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+
+num = atoi(argv[1]);
+if (num < 0)
+{
+printf("0\n");
+}
+for (i = 0; num != 0; i++)
+
+if (num > 25)
+{
+cents =  num / 25
+
+else
+if (num > 10)
+cents = num / 10 ;
+else
+if (num < 10 && num > 5)
+cents = num \ 5;
+else 
+(num > 0 && num < 5)
+cents = num / 1;
+}
+
+print
