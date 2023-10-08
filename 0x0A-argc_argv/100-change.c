5abf28535fcef849cdc18c849a375495fc251944
GODSPE1 <godspy18@gmail.com>
2023-10-07 23:21:32 -0700

    updated

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * main - calculate number of arguments passed into it
+ * @argc: number of argumens
+ * @argv: array of the arguments
+ * Return: Always 0.
  */
-
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
+int a = 0;
 int q = 0;
 int d = 0;
-int n = ;
+int n = 0;
 int p = 0;
-int cents = 0;
+int num = 0;
 
-if (argc < 2)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
 
 num = atoi(argv[1]);
+
 if (num < 0)
-{
 printf("0\n");
+
+q = num / 25;
+num = num % 25;
+
+d = num / 10;
+num = num % 10;
+
+n = num / 5;
+num = num % 5;
+
+a = num / 2;
+num = num % 2;
+
+p = num;
+printf("%d\n", (a + q + d + n + p));
+
+return (0);
 }
-for (i = 0; num != 0; i++)
-
-if (num > 25)
-{
-cents =  num / 25
-
-else
-if (num > 10)
-cents = num / 10 ;
-else
-if (num < 10 && num > 5)
-cents = num \ 5;
-else 
-(num > 0 && num < 5)
-cents = num / 1;
-}
-
-print
