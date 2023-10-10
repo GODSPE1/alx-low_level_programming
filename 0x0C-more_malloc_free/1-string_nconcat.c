799c0b0e06b9839373588575a04152fb88a485c4
GODSPE1 <godspy18@gmail.com>
2023-10-09 19:18:12 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,37 +10,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	int len1;
-	unsigned int len2, c, k, length;
+	unsigned int i, len;
 
-	if (s1 == NULL)
+	if(s1 == NULL)
 		s1 = "";
-	else
-		for (len1 = 0; s1[len1] != '\0'; len1++)
 
-			if (s2 == NULL)
-				s2 = "";
-			else
-				for (len2 = 0; s2[len2] != '\0'; len2++)
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+		len++;
 
-					if (n >= len2)
-						length = len2 + 1;
-					else
-						length = len1 + n + 1;
-
-	p = malloc(length * sizeof(char));
+	p = malloc((len + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (k = 0; s1[k] != '\0'; k++)
+	len = 0;
+	for (i = 0; s1[i]; i++)
 	{
-		p[k] = s1[k];
+		p[len++] = s1[i];
 	}
 
-	for (c = 0; c < n && s2[len1] != '\0'; c++)
+	for (i = 0; s2[i] && i < n; i++)
 	{
-		p[len1 + c] = s2[c];
+		p[len++] = s2[i];
 	}
-	p[length] = '\0';
+	p[len] = '\0';
 
 	return (p);
 }
