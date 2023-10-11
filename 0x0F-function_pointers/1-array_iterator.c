b1a5f622e96c22cf25de909f27447aa272442fab
GODSPE1 <godspy18@gmail.com>
2023-10-11 14:33:18 -0700

    passing function as parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * array_iterator - iterates through the array
+ * @array: A pointer to an array of integers.
+ * @size: The number of elements in the array
+ * @action: A function pointer that can point to a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if  (action != NULL)
+		{
+			action(array[i]);
+		}
+	}
+
+}
