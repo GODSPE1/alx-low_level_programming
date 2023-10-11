eec9c1bbd8fbf8838ef6a41732aa59482728c94a
GODSPE1 <godspy18@gmail.com>
2023-10-10 19:29:46 -0700

    functio that free a space

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * free_dog - free the malloc dog_t
+ * @d: ponter to dog_t structure
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(my_dog);
+
+	return (0);
+}
