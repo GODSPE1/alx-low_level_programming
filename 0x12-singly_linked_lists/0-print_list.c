c2701153765e73abdce9e2812a5c973c9dc13b84
GODSPE1 <godspy18@gmail.com>
2023-10-20 05:03:59 -0700

    Renamed the directory

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list
+ * @h: a pointer to a list_t structure
+ *
+ * Return: the count of node at the end of the function
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int  count; = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
