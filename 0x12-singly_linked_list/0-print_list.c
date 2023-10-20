2b4f39be5e0e641d7c51953a6db6f16d789a162b
GODSPE1 <godspy18@gmail.com>
2023-10-19 17:11:35 -0700

    creating all my .c files

---
+++ 0x12-singly_linked_list/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list 
+ * @list_t: a pointer to a structure
+ *
+ * Return: the count of node at the end of the function
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int  count;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
