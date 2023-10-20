1ca968b5002379fafccb54774ac7f68aef28105b
GODSPE1 <godspy18@gmail.com>
2023-10-19 17:29:27 -0700

    debug

--- 0x12-singly_linked_list/lists.h
+++ 0x12-singly_linked_list/lists.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 typedef struct list_t
@@ -9,7 +10,7 @@ typedef struct list_t
 	struct list_t *next;
 }list_t;
 
-
+int _putchar(char c);
 size_t print_list(const list_t *h);
 
 #endif
