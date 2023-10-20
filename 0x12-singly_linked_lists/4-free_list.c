a5b491a75bc9364dac7252ea154b74f0d6046843
GODSPE1 <godspy18@gmail.com>
2023-10-20 16:37:25 -0700

    free list function

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: list to be freed
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t * temp;
+
+	while (head != NULL)
+	{
+		temp = headnext;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+
+}
