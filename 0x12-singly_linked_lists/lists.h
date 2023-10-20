ba2a069ea65ae1810000f46d74ceb817a042cfec
GODSPE1 <godspy18@gmail.com>
2023-10-20 10:22:08 -0700

    comment for list_t structure

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,15 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
+
+/**
+ * struct list_t singly linked list
+ * @str: dynamically allocated string
+ * @len: length of the string
+ * @next: self referencial node that help to points to the next node
+ */
 typedef struct list_t
 {
 	char *str;
@@ -13,5 +21,6 @@ typedef struct list_t
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+
 
 #endif
