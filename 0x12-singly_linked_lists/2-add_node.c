44dc1d3f49a851ae98831440037f6a133d26b3f0
GODSPE1 <godspy18@gmail.com>
2023-10-20 16:08:55 -0700

    add node function

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - adds newnode to the list
+ * @head: current head of the list
+ * @str: string to be stored in the new node
+ *
+ * Return: address of the new node on success
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len = 0;
+	list_t *newnode;
+
+	if (head == NULL)
+		return (0);
+
+	newnode = (list_t *)malloc(sizeof(list_t));
+	if (newnode != NULL)
+	{
+		newnode->str = strdup(str);
+		newnode->len  = len;
+		newnode->next = *head;
+		*head = newnode;
+	}
+	else 
+		return (NULL);
+
+	return (*head);
+}
