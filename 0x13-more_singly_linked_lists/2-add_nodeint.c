9352e3a19aa238015e2af67620018fb4870a4281
GODSPE1 <godspy18@gmail.com>
2023-10-23 16:04:31 -0700

    free newnode when it fails

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode = malloc(sizeof(listint_t));
 
 	if (newnode == NULL)
+	{
+		free(newnode);
 		return (NULL);
+	}
 	newnode->n = n;
 	newnode->next = NULL;
 
