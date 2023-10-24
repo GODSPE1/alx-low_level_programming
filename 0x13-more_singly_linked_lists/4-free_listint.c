7631658818fb64f0523de22435311919b770b447
GODSPE1 <godspy18@gmail.com>
2023-10-24 16:33:21 -0700

    debug

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,13 @@
 #include "lists.h"
 /**
  * free_listint - free the node allocated space
- * @head: pointer to a poiner that points to the head of a linked list
+ * @head: head of a linked list
  *
  * Return: Nothing.
  */
 void free_listint(listint_t *head)
 {
 	listint_t *init;
-
-		if (head == NULL)
-			return (0);
 
 	while (head != NULL)
 	{
