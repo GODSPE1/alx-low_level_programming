9434e3026f3c090f7ae960bccadf24f89ca757c4
GODSPE1 <godspy18@gmail.com>
2023-10-25 12:35:05 -0700

    set the two pointers to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	temp = *head;
+	while (temp != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		temp = *head;
+		temp = temp->next;
+		free(*head);
+		*head = temp;
 	}
 }
