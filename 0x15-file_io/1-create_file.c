136e98173c6541e620d595815a361ed2bea98413
GODSPE1 <godspy18@gmail.com>
2023-10-30 19:19:06 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: file descriptor used or -1 upon failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -12,11 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];) 
+		for (length = 0; text_content[length];) 
 			length++;
 	}
-	fd = open(filename, O_CREAT | O_RWWR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	byte_write = write(fd, text_content, length);
+
 	if (fd == -1 || byte_write == -1)
 		return (-1);
 
