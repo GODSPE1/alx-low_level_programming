fcdb0494c2aec5fccd7df5902afff173eda1c111
GODSPE1 <godspy18@gmail.com>
2023-10-31 06:48:00 -0700

    debug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: the name of the file
@@ -12,7 +13,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, byte_write;
 
 	if (filename == NULL)
-		return (-1);
+		{
+return (-1);
+}
 
 	if (text_content == NULL)
 	{
