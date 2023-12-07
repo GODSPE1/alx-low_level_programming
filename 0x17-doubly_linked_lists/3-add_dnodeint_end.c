1ec7263aa3b2ef864fd4ec9c8a267a8fc0cc66e0
GODSPE1 <godspy18@gmail.com>
2023-12-07 15:12:39 -0800

    add nod at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - add node at the en
+ * @n: value
+ *
+ * Return: Address of the new element.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode, *temp;
+
+	newNode = malloc(sizeof(dlistint_t))
+		newNode->n = n;
+	newNode->next = NULL;
+	if (*head ==NULL)
+	{
+		newNode->prev = NULL;
+		*head = newNode;
+		return newNode;
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = newNode;
+	newNode->prev = temp;
+	return *head;
+
+}
+
