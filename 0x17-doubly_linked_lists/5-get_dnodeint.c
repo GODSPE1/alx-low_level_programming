747f91c97f2337ab284a66ba5d1f4a10c4fcf725
Godspower Nwankwo <107337599+GODSPE1@users.noreply.github.com>
2023-12-08 16:35:40 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,60 +1,33 @@
 #include "lists.h"
+
 /**
- * get_dnodeint_at_index -  returns the nth node of a dlistint_t linked list.
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
  * @head: pointer of the head of the list
- * @index of node to find
- * Return: the nth index, return NULL if the nodes does not exit
+ * @index: index of the node to find
+ * Return: the nth node, return NULL if the node does not exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-<<<<<<< HEAD
-	dlistint_t *newnode;
-	unsigned int count;
-=======
-        dlistint_t *newnode;
-        unsigned int count;
->>>>>>> c45f2ac7b4dec0ecf615d1ec92e1f0cc0ae25bb7
+    dlistint_t *current;
+    unsigned int count = 0;
 
-        if (head == NULL)
-                return (NULL);
-
-<<<<<<< HEAD
-	while (head->prev != NULL)
-		head = head-prev;
-
-	newnode = head;
-
-	while (newnode != NULL)
-	{
-		if (count == index)
-<<<<<<< HEAD
-		{
-			return (newnode);
-		}
-		count++;
-		newnode = newnode->next;
-=======
-			break;
-		head = head->next;
->>>>>>> 212f33f48a81f3445789be3703edacd9b8de01a9
-	}
-	return (head);
-=======
-        while (head->prev != NULL)
-                head = head-prev;
-
-        newnode = head;
-
-        while (newnode != NULL)
-        {
-                if (count == index)
-                {
-                        return (newnode);
-                }
-                count++;
-                newnode = newnode->next;
-        }
+    if (head == NULL)
         return (NULL);
-	
->>>>>>> c45f2ac7b4dec0ecf615d1ec92e1f0cc0ae25bb7
+
+    while (head->prev != NULL)
+        head = head->prev;
+
+    current = head;
+
+    while (current != NULL)
+    {
+        if (count == index)
+        {
+            return current;
+        }
+        count++;
+        current = current->next;
+    }
+
+    return NULL;
 }
