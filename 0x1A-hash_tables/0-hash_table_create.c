784b2f20ed1baa162cf40ea5317a0e19f0f87660
GODSPE1 <godspy18@gmail.com>
2023-12-22 07:21:17 -0800

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,30 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table *hash_table;
-	unsigned long int i;
+	 hash_table_t *new_table;
+        unsigned long int i;
 
-	hash_table = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t));
-	if (hash_table->array == NULL);
-	{
-		free(hash_table);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+        {
+                return (NULL);
+        }
+
+	new_table->array = malloc(size * sizeof(hash_node_t));
+
+        
+	if (new_table->array == NULL)
+        {
+                free(new_table);
+                return (NULL);
+        }
+	
+	new_table->size = size;
+
+        for (i = 0; i < size; i++)
+        {
+                new_table->array[i] = NULL;
+        }
+
+        return (new_table);
 }
