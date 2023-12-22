4e95c4d81d8ed1d6cf13191be531ee2a16aa0e58
GODSPE1 <godspy18@gmail.com>
2023-12-22 07:28:37 -0800

    check for betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_t *hash_table_create - create a hash able
+ * @size: size of the array
  *
- *
- *
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	 hash_table_t *new_table;
-        unsigned long int i;
+	hash_table_t *new_table;
+	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
-        {
-                return (NULL);
-        }
+	{
+		return (NULL);
+	}
 
 	new_table->array = malloc(size * sizeof(hash_node_t));
 
-        
+
 	if (new_table->array == NULL)
-        {
-                free(new_table);
-                return (NULL);
-        }
-	
+	{
+		free(new_table);
+		return (NULL);
+	}
+
 	new_table->size = size;
 
-        for (i = 0; i < size; i++)
-        {
-                new_table->array[i] = NULL;
-        }
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
 
-        return (new_table);
+	return (new_table);
 }
