a5a253a9705de9f0c21afbe05d3faa5ea231e14b
GODSPE1 <godspy18@gmail.com>
2024-04-13 22:53:11 +0100

    remove uneccesary space

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,10 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches for a value in an array of integers using
  * the Linear search algorithm.
  * @array: is a pointer to the first element of the array to search in.
  * @size: is the number of elements in the array.
  * @value: is the value to search for.
- *
  * Return: The first index where value is located.
  * If value is not present in array or if array is NULL, return -1.
  */
